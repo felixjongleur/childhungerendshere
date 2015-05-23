caf200f1007382ea8fba0a1e329405019fa829ea
moejello <mello.10@osu.edu>
2015-05-22 20:36:41 -0400

    Update utils.rb
    added how to run on sauce

--- features/classes/helper_classes/utils.rb
+++ features/classes/helper_classes/utils.rb
@@ -1,6 +1,18 @@
 module Utils
   def open_browser
     @browser = Watir::Browser.start('http://www.childhungerendshere.com/', :chrome)
+    
+    #comment out the line above and uncomment below to run on saucelabs
+    # username = 'moejello'
+    # access_key = '9150097b-deef-417d-9e9f-fff2b546d980'
+    #
+    # caps = {:name => 'Testing in the Sauce Cloud', :platform => 'WINDOWS 7', :version => '33', :browserName => 'firefox'}
+    #
+    # @browser = Watir::Browser.new(:remote,
+    #                               :url => "http://#{username}:#{access_key}@ondemand.saucelabs.com:80/wd/hub",
+    #                               :desired_capabilities => caps)
+    # @browser.goto('http://www.childhungerendshere.com/')
+    # @browser
   end
 
   def start_application
