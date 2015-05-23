382c224276d683fad9e8eb61c49678ec8a3e5f0c
moejello <mello.10@osu.edu>
2015-05-22 20:34:23 -0400

    Update general_steps.rb

--- features/step_definitions/general_steps.rb
+++ features/step_definitions/general_steps.rb
@@ -5,23 +5,23 @@ end
 And(/^I enter a new code$/) do
   start = Time.now
     30.times do
-    Watir::Wait.until { @page.input0_element.enabled? }
+    Watir::Wait.until { @page.input0_element.visible? }
     @page.input0 = '55950954'
     @page.add_another
 
-    Watir::Wait.until { @page.input1_element.enabled? }
+    Watir::Wait.until { @page.input1_element.visible? }
     @page.input1 = '55950954'
     @page.add_another
 
-    Watir::Wait.until { @page.input2_element.enabled? }
+    Watir::Wait.until { @page.input2_element.visible? }
     @page.input2 = '55950954'
     @page.add_another
 
-    Watir::Wait.until { @page.input3_element.enabled? }
+    Watir::Wait.until { @page.input3_element.visible? }
     @page.input3 = '55950954'
     @page.add_another
 
-    Watir::Wait.until { @page.input4_element.enabled? }
+    Watir::Wait.until { @page.input4_element.visible? }
     @page.input4 = '55950954'
     @page.submit
     @page.clear_cookies
